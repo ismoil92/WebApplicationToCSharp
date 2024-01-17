f4d6d917ddb461d75a1973ee0ec5a5d7e69718fd
Ismoil Muhammadiev <ism0il.muhammadiev92@gmail.com>
2024-01-17 11:54:28 +0500

    Первая домашняя работа выполнена.

---
+++ ClassLibrary.ContextAndRepository/Models/Base/BaseModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ClassLibrary.ContextAndRepository.Models.Base;
+
+public class BaseModel
+{
+    public int ID { get; set; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+}
